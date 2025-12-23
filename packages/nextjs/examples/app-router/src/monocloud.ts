82ecd7dd350ce0928c7e1b281deec317e4003d39
Vishal Rastogi <maganuk@hotmail.com>
2025-12-24 01:57:44 +0530

    Fix Example

--- packages/nextjs/examples/app-router/src/monocloud.ts
+++ packages/nextjs/examples/app-router/src/monocloud.ts
@@ -1,9 +1,3 @@
 import { MonoCloudNextClient } from '@monocloud/auth-nextjs';
 
-export const monoCloud = new MonoCloudNextClient({ resources: [{
-  resource: 'https://api.monocloud.com/admin',
-  scopes: 'admin'
-}, {
-  resource: 'https://api.monocloud.com/identity',
-  scopes: 'identity'
-}] });
+export const monoCloud = new MonoCloudNextClient();
